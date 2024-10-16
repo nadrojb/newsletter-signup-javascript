7fd5952d967c4ca35d9f9f54b0e211a0cbeaf69f
jordan_brewer <jordanbrewer1994@gmail.com>
2024-10-16 21:17:22 +0100

    changes the layout of invalid email error message
    still need to work on changing the backgrounf of the input bar to red when an !valid email is entered

--- script.js
+++ script.js
@@ -5,9 +5,32 @@ let form = document.querySelector("form");
 function validateEmail(email) {
   const validReg =
     /^([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])/;
+
+  const warningMessage = document.querySelector(".invalid-email-warning");
+  const emailInput = document.querySelector(".email-input");
+  const checkMark = document.querySelector(".check");
+
+  emailInput.classList.add("border", "bg-slate-100");
+  emailInput.classList.remove(
+    "border-green-700",
+    "border-2",
+    "border-red-600",
+    "bg-red-200"
+  );
+  warningMessage.classList.add("hidden");
+  warningMessage.classList.remove("block");
+
   if (email.match(validReg)) {
-    alert("valid email address");
-  } else alert("invalid email");
+    emailInput.classList.remove("border");
+    emailInput.classList.add("border-2", "border-green-700");
+    checkMark.classList.remove("hidden");
+    checkMark.classList.add("inline-block");
+  } else if (email == "" || !email.match(validReg)) {
+    emailInput.classList.remove("border", "bg-slate-100");
+    emailInput.classList.add("border-2", "border-red-600", "bg-red-200");
+    warningMessage.classList.remove("hidden");
+    warningMessage.classList.add("block");
+  }
 }
 
 form.addEventListener("submit", (event) => {
